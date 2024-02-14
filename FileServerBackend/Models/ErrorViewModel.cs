f80d6ca8aef1e5ea0aca7a2001416b72cbe09916
Bryan Fraschetti <bryan.fraschetti@gmail.com>
2024-02-14 17:25:07 -0500

    Web App MVC Project Template

---
+++ FileServerBackend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FileServerBackend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
